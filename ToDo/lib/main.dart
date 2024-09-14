004a9b58e48ad5de4a35020cdccd3f690c9dce2f
Sergio Fernandez Davila <killeranime666@gmail.com>
2024-09-13 22:53:19 -0400

    Adicion de dependencias y avance de modulo futuro en el main

--- ToDo/lib/main.dart
+++ ToDo/lib/main.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:todo/views/home/home.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
+Future<void> main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter<Task>(TaskAdapter());
 
-void main() {
-  runApp(const MyApp());
+  var box = await Hive.openBox<Task>('tasksBox');
+
+  box.values.forEach((task) {
+    if(task.createdAtTime.day != DateTime.now().day) {
+      task.delete();
+    } else {}
+  });
+
+  runApp(BaseWidget(child: const MyApp()
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.green
-      ),
-      home: const Home(),
-    );
+    return const Placeholder();
   }
 }

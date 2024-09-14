f0f219a570cb2f59c03f0aae6484e94054ca19e7
Sergio Fernandez Davila <killeranime666@gmail.com>
2024-09-14 11:24:14 -0400

    finalizado el apartado de tareas

---
+++ ToDo/lib/views/home/widgets/task_widget.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../../models/task.dart';
+import '../../../utils/colors.dart';
+import '../../../views/tasks/task_view.dart';
+
+class TaskWidget extends StatefulWidget {
+  const TaskWidget({Key? key, required this.task}) : super(key: key);
+
+  final Task task;
+
+  @override
+  _TaskWidgetState createState() => _TaskWidgetState();
+}
+
+class _TaskWidgetState extends State<TaskWidget> {
+  TextEditingController taskControllerForTitle = TextEditingController();
+  TextEditingController taskControllerForSubtitle = TextEditingController();
+
+  @override
+  void initState() {
+    taskControllerForTitle.text = widget.task.title;
+    taskControllerForSubtitle.text = widget.task.subtitle;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    taskControllerForTitle.dispose();
+    taskControllerForSubtitle.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            CupertinoPageRoute(
+                builder: (ctx) => TaskView(
+                  taskControllerForTitle: taskControllerForTitle,
+                  taskControllerForSubtitle: taskControllerForSubtitle,
+                  task: widget.task,
+                ),
+            ),
+        );
+      },
+
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 600),
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        decoration: BoxDecoration(
+          color: widget.task.isCompleted
+              ? const Color.fromARGB(154, 119, 144, 229)
+              : Colors.white,
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              offset: const Offset(0, 4),
+              blurRadius: 10,
+            )
+          ]
+        ),
+        child: ListTile(
+          leading: GestureDetector(
+            onTap: () {
+              widget.task.isCompleted = !widget.task.isCompleted;
+              widget.task.save();
+            },
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 600),
+              decoration: BoxDecoration(
+                color: widget.task.isCompleted
+                    ? MyColors.primaryColor
+                    : Colors.white,
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 0.8,
+                )
+              ),
+              child: const Icon(
+                Icons.check,
+                color: Colors.white,
+              ),
+            ),
+          ),
+
+          title: Padding(
+            padding: const EdgeInsets.only(bottom: 8, top: 3),
+            child: Text(
+              taskControllerForTitle.text,
+              style: TextStyle(
+                color: widget.task.isCompleted
+                    ? MyColors.primaryColor
+                    : Colors.black,
+                fontWeight: FontWeight.w500,
+                decoration: widget.task.isCompleted
+                    ? TextDecoration.lineThrough
+                    : null,
+              ),
+            ),
+          ),
+
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                taskControllerForSubtitle.text,
+                style: TextStyle(
+                  color: widget.task.isCompleted
+                      ? MyColors.primaryColor
+                      : const Color.fromARGB(255, 164, 164, 164),
+                  fontWeight: FontWeight.w300,
+                  decoration: widget.task.isCompleted
+                      ? TextDecoration.lineThrough
+                      : null,
+                ),
+              ),
+
+              Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                    padding: const EdgeInsets.only(
+                      top: 10,
+                      bottom: 10
+                    ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        DateFormat('hh:mm a')
+                            .format(widget.task.createdAtTime),
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: widget.task.isCompleted
+                              ? Colors.white
+                              : Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
